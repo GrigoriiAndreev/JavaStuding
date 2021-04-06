573db81c437a5f2a607da8134221a8de6580fdaf
GrigoriiAndreev <71266142+GrigoriiAndreev@users.noreply.github.com>
2021-04-06 12:18:02 +0300

    Delete task01 directory

--- task01/task0116/Solution.java
+++
@@ -1,9 +0,0 @@
-package task01.task0116;
-
-public class Solution {
-    public static void main(String[] args) {
-        String name = "Amigo";
-        String text = "Hello, " + name + "!";
-        System.out.println(text);
-    }
-}
